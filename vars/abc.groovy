0ec40e9e74b846ba15b20d8d1da22a3b80e1a57c
krishnain <86010658+krishnain@users.noreply.github.com>
2022-01-18 11:32:42 +0530

    Update abc.groovy

--- vars/abc.groovy
+++ vars/abc.groovy
@@ -9,3 +9,8 @@ def newMaven()
   sh label: '', script: 'mvn package'
 }
 
+def newDeploy(ip)
+{
+  sh 'scp /home/ubuntu/.jenkins/workspace/Sample/webapp/target/webapp.war ubuntu@"${ip}":/var/lib/tomcat9/webapps/qaapp.war' 
+}
+
